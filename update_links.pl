93a117850071adbe9fcd32e3c82751c374aa6e46
Kursad Oney <kursad.oney@freescale.com>
2012-04-03 17:53:25 -0400

    update_links: Added a script to fix the dotfiles in $HOME
    This script goes through the files in this directory and finds
    the equivalent dot files in $HOME. If there is a link from $HOME
    to the file here, we leave it alone. Otherwise, we save the original
    and create a link to the current version here.

---
+++ update_links.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+use File::Copy "mv";
+use Env qw(HOME);
+use Cwd 'abs_path';
+
+sub save_orig {
+	my ($df, $f) = @_;
+	print "[ MV ] $df orig/$f\n";
+	mv ($df, "./orig/$f") or die $!;
+}
+
+sub create_link {
+	my ($df, $f) = @_;
+	print "[ LN ] $f $df\n";
+	symlink (abs_path($f), $df) or die $!;
+}
+
+sub check_skip {
+	my $f = shift;
+	return 1 if (($f =~ /^\./) ||
+				 ($f eq basename($0)) ||
+				 ($f eq "orig"));
+	return 0;
+}
+
+# If ./orig doesn't exist, create it
+#
+# For each file in this directory:
+#	SKIP this file, orig, anything that starts with a '.'
+# 	NEXT if there is a link from $HOME/.$file to pwd/$file
+# 	MOVE the file to pwd/orig
+# 	LN   from $HOME/$file to pwd/$file
+
+mkdir "./orig",0755 if (! -e "./orig");
+
+opendir (DIR, '.') or die $!;
+
+while (my $file = readdir(DIR)) {
+	next if check_skip ("$file");
+
+	print "[ PROC ] $file\n";
+
+	my $dotfile = "$HOME" . "/." . "$file";
+	# does the dot file exist in $HOME ? NO, create_link()
+	if (! -e "$dotfile") {
+		create_link ($dotfile, $file);
+		next;
+	}
+	# YES: is it a link to the file here ? NO, save_orig(), create_link()
+	if ( -l "$dotfile" ) {
+		# If it is a link to us, next
+		# Else, break the link, save the target in orig, create_link
+		print "[ INFO ] $dotfile is a link to" . readlink ($dotfile) . "\n";
+		next if ( readlink ($dotfile) eq abs_path($file) );
+
+		save_orig (readlink ($dotfile), $file);
+		unlink ($dotfile) or die $!;
+		create_link ($dotfile, $file);
+	} else {
+		save_orig ($dotfile, $file);
+		create_link ($dotfile, $file);
+	}
+}
