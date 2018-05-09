70a2de8f801ebb5994dbfa7e799327d408156468
Kursad Oney <kursad.oney@broadcom.com>
2018-05-09 08:04:10 -0700

    brcm files: Major rework to consolidate and add new features.
    Multiple things in one commit (yeah, not ideal) but here they
    are:
    
    * Change export PATH's to pathmunge equivalents.
    * Make setup_u_boot call setup_environment
    * Replace dashes in bashrc functions with underscores. The dashes
      are not proper syntax and cannot be exported.
    * Set SUBVERSIONVER=1.7.8 per BRCM guidelines.
    * Add ksub function to use the LFS for commands.
    * Consolidate rtp.sh and sanjose.sh. These scripts now only set
      rtp or sanjose-specific variables (such as PHOME) and source
      brcm.sh which uses those variables to generate common functions
      and settings.

---
+++ host/brcm.sh
@@ -0,0 +1,62 @@
+function setup_environment() {
+
+	if [ "$#" -lt 2 ]; then
+		echo "${FUNCNAME[0]} arch glibc_ver (arch=arm, aarch64; glibc_ver=22, 24)"
+		return 0
+	fi
+
+	arch=${1}
+	glibc_ver=${2}
+
+	TC_BASE_BASE=/projects/${ORG}/tools/linux
+	GCC=crosstools-${arch}-gcc-5.3
+	KERNEL=linux-4.1
+	GLIBC=glibc-2.${glibc_ver}
+	BINUTILS=binutils-2.25
+
+	export TOOLCHAIN_BASE=${TC_BASE_BASE}/BCG
+	export LD_LIBRARY_PATH=${TOOLCHAIN_BASE}/${GCC}-${KERNEL}-${GLIBC}-${BINUTILS}/usr/lib:$LD_LIBRARY_PATH
+	pathmunge ${TC_BASE_BASE}/hndtools-armeabi-2013.11/bin after
+	# optional
+	pathmunge ${TOOLCHAIN_BASE}/crosstools-aarch64-${GCC}-${KERNEL}-${GLIBC}-${BINUTILS}/usr/bin/ after
+}
+
+function setup_u_boot() {
+
+	if setup_environment $@; then
+		export CROSS_COMPILE=${TOOLCHAIN_BASE}/${GCC}-${KERNEL}-${GLIBC}-${BINUTILS}/usr/bin/aarch64-buildroot-linux-gnu-
+		export ARCH=arm64
+	fi
+}
+
+function populate_impl51() {
+	cd bcmdrivers/broadcom/net/wl/impl51 &&
+	hnd -V scm co -t KUDU_BRANCH_17_10 -d . linux-4.1.0-router &&
+	cd ../../../../../
+}
+
+function ksub() {
+	/tools/bin/bsub -q ${QUEUE} -R osmajor='RHEL6 span[hosts=1]' -sp 120 -n 8 -Is "$@"
+}
+
+alias cdp='cd $PHOME'
+export SUBVERSIONVER=1.7.8
+
+pathmunge ${PHOME}/bin
+pathmunge ${PHOME}/install/bin
+pathmunge /tools/bins after
+pathmunge /tools/nwsoft/bin after
+pathmunge /projects/${ORG}/tools/bin after
+
+unset PS1
+unset GIT_PS1_SHOWDIRTYSTATE
+GIT_PROMPT_FETCH_REMOTE_STATUS=0
+GIT_PROMPT_SHOW_UPSTREAM=0
+GIT_PROMPT_SHOW_UNTRACKED_FILES=no
+GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0
+GIT_PROMPT_THEME=Single_line_Solarized
+source ~/.bash-git-prompt/gitprompt.sh
+
+[[ -f ~/.git-completion.bash ]] && source ~/.git-completion.bash
+
+export BASH_ENV=$HOME/.bashrc
