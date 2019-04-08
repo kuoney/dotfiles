63a0813ac7af5f45237b47ee0c6f830bd82833fb
Kursad Oney <kuoney@gmail.com>
2019-04-08 14:34:59 -0400

    ub-work: Add new host file to source.
    ub-work is a 64-bit 18.04 Ubuntu VM.

---
+++ host/ub-work.sh
@@ -0,0 +1,8 @@
+export ORG=hnd
+export PHOME="${HOME}/code/work"
+
+source $HOME/.dotfiles/host/brcm.sh
+
+# we have ccache for host enabled on this host so turn it
+# on for everyone.
+pathmunge /usr/lib/ccache/ before
