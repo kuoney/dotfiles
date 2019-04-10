e87f31ef4c59c8eedf2b16fe9e06ab5b5a1cbd67
Kursad Oney <kuoney@gmail.com>
2019-04-09 21:45:16 -0400

    ub-work host file: Add brcm tools to path

--- host/ub-work.sh
+++ host/ub-work.sh
@@ -6,3 +6,6 @@ source $HOME/.dotfiles/host/brcm.sh
 # we have ccache for host enabled on this host so turn it
 # on for everyone.
 pathmunge /usr/lib/ccache/ before
+
+# for hnd, precommit etc. tools
+pathmunge /opt/bca/bin/ after
