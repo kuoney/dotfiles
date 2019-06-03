8b90cff232396daea2fb3b129e159f12fc9d63f3
Kursad Oney <kursad.oney@broadcom.com>
2019-06-03 01:35:34 -0700

    sanjose: Adjust PHOME.
    If the faster disk exists, set the PHOME there. Otherwise default
    to NFS.

--- host/sanjose.sh
+++ host/sanjose.sh
@@ -1,5 +1,10 @@
 export ORG=bca
 export QUEUE=sj-bcasw
-export PHOME='/projects/bcawlan_ext02/work/ko889424'
+
+if [ -d /auto/workspace_02/ko889424/ ]; then
+	export PHOME='/auto/workspace_02/ko889424/'
+else
+	export PHOME='/projects/bcawlan_ext02/work/ko889424'
+fi
 
 source $HOME/.dotfiles/host/brcm.sh
