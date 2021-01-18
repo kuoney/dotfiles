972ba7869fc1098ffa25283a37cb8c7bced342f6
Kursad Oney <kursad.oney@broadcom.com>
2021-01-18 12:45:17 -0500

    uvm: Set JAVA_HOME for bbsx.
    Without this variable, BBSX does not work.

---
+++ host/uvm.sh
@@ -0,0 +1,16 @@
+export ORG=hnd
+export PHOME="${HOME}/code/"
+
+source $HOME/.dotfiles/host/brcm.sh
+
+# we have ccache for host enabled on this host so turn it
+# on for everyone.
+# pathmunge /usr/lib/ccache/ before
+
+# for hnd, precommit etc. tools
+pathmunge /opt/bca/bin/ after
+pathmunge /home/ko889424/code/git/esdk-misc-utils after
+# export TOOLCHAIN_BASE=/opt/cached
+
+# BBSX requires JAVA_HOME to be set
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
