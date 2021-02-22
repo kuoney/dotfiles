098ff7bd328a3de073261a3a84e69ce1e3d3dac4
Kursad Oney <kursad.oney@broadcom.com>
2021-02-22 11:25:36 -0500

    brcm: Add ackwl, cleanup.
    Remove unused functions.
    
    Add ackwl which finds the active IMPL and searches that directory
    with ack.

--- host/brcm.sh
+++ host/brcm.sh
@@ -1,23 +1,14 @@
-function setup_uboot() {
-
-	GCC=crosstools-${1-aarch64}-gcc-${2-5.5}
-	KERNEL=linux-${3-4.1}
-	GLIBC=glibc-${4-2.26}
-	BINUTILS=binutils-${5-2.28.1}
-
-	export TOOLCHAIN_BASE=/opt/toolchains
-	# optional to use other tools like objcopy
-	pathmunge ${TOOLCHAIN_BASE}/${GCC}-${KERNEL}-${GLIBC}-${BINUTILS}/usr/bin/ after
-
-	# optional, for building u-boot separately
-	[[ "${1-aarch64}" == "aarch64" ]] && CC_PREFIX=buildroot-linux-gnu- || CC_PREFIX=buildroot-linux-gnueabi-
-	export CROSS_COMPILE=${1-aarch64}-${CC_PREFIX}
-}
-
-# export these functions to be used in other scripts
-export -f setup_uboot
-
 alias cdp='cd $PHOME'
+
+ackwl() {
+	wltop=bcmdrivers/broadcom/net/wl
+	wlimpl=$(find "$wltop" -maxdepth 2 -type d -name main -printf "%h\n")
+	if [ -z "$wlimpl" ]; then
+		echo "no active wlimpl"
+	else
+		ack "${@:1}" "$wlimpl"
+	fi
+}
 
 unset GIT_PS1_SHOWDIRTYSTATE
 GIT_PROMPT_ONLY_IN_REPO=1
