6211d61944f2c2e3dda60128eef9866964758d23
Kursad Oney <kursad.oney@broadcom.com>
2021-10-28 09:20:06 -0400

    uvm: Set UTILSDIR for FD builds.

--- host/uvm.sh
+++ host/uvm.sh
@@ -1,8 +1,6 @@
 export ORG=hnd
 export PHOME="${HOME}/code/"
 
-source $HOME/.dotfiles/host/brcm.sh
-
 # we have ccache for host enabled on this host so turn it
 # on for everyone.
 # pathmunge /usr/lib/ccache/ before
@@ -14,3 +12,6 @@ export TOOLCHAIN_BASE=/opt/cached
 
 # BBSX requires JAVA_HOME to be set
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# To build FD, we need to set UTILSDIR
+export UTILSDIR=$HOME/bin
