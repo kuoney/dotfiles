5a9c8f210e5179986a03ac6126141eb98d4e5334
Kursad Oney <kursad.oney@broadcom.com>
2024-10-11 16:24:09 -0400

    brcm: Explicitly set ccache size, use newer htop.

--- host/brcm.sh
+++ host/brcm.sh
@@ -28,3 +28,6 @@ source ${PHOME}/git/esdk-misc-utils/build/esdk.sh
 [ -d ${PHOME}/tools/bin ] &&		pathpurge ${PHOME}/tools/bin && pathmunge ${PHOME}/tools/bin before
 [ -d /projects/hnd/tools/linux/bin/ ] &&	pathmunge /projects/hnd/tools/linux/bin/ after
 export PAHOLEVER=1.22
+export HTOPVER=2.2.0
+
+ccache -M 8.0G
